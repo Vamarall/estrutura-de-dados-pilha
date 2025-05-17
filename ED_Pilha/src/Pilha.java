408abc35bdbbad4e168e8844729aeaa7c42f93fb
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-05-17 19:44:17 -0300

    Criados metodos obterNumeroItens e inverter

--- ED_Pilha/src/Pilha.java
+++ ED_Pilha/src/Pilha.java
@@ -5,7 +5,8 @@ public class Pilha<E> {
 
     public Pilha() {
         Celula<E> sentinela = new Celula<>();
-        sentinela = topo = fundo;
+        this.fundo = sentinela;
+        this.topo = sentinela;
     }
 
     public boolean isEmpty() {
@@ -14,8 +15,9 @@ public class Pilha<E> {
 
     public void push(E item) {
         Celula<E> novaCelula = new Celula<E>(item, topo);
-    }
+        this.topo = novaCelula;
 
+    }
 
     public E pop() {
         if (isEmpty()) {
@@ -25,16 +27,63 @@ public class Pilha<E> {
         topo = topo.getProximaCelula();
         return item;
 
-
     }
 
-    public E consultarTopo(){
-        if(isEmpty()){
+    public E consultarTopo() {
+        if (isEmpty()) {
             return null;
         }
 
         E item = topo.getItem();
         return item;
+    }
+
+    public void concatenar(Pilha<E> pilha) {
+        if (this == pilha) {
+            System.out.println("Nao é possivel concatenar a pilha com ela mesma");
+            return;
+        }
+        Pilha<E> auxPilha = new Pilha<>();
+        Celula<E> aux = pilha.topo;
+
+        // Copia os elementos da outra pilha para a auxiliar (revertendo a ordem)
+        while (aux != pilha.fundo) {
+            auxPilha.push(aux.getItem());
+            aux = aux.getProximaCelula();
+        }
+
+        // Agora insere na pilha atual, preservando a ordem da original
+        while(!auxPilha.isEmpty()){
+            this.push(auxPilha.pop());
+        }
+    }
+
+    public int obterNumeroItens(){
+        if(isEmpty()){return 0;}
+        Celula<E> aux = this.topo;
+        int cont = 0;
+
+        while(aux != fundo){ //Para no fundo (sentinela)
+            cont ++;
+            aux = aux.getProximaCelula();
+
+        }
+        return cont;
+    }
+
+    public void inverter(){
+        if(isEmpty()){ return ;}
+
+        Pilha<E> pilhaAux = new Pilha<>();
+        
+
+        while(!this.isEmpty()){
+           pilhaAux.push(this.pop());
+        }
+
+        while(!pilhaAux.isEmpty()){
+            this.push(pilhaAux.pop());
+        }
     }
 
 }
