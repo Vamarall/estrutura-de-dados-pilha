d0abc6465ade14388354ca3f619ca68b7948b81c
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-05-17 14:20:09 -0300

    Criada classe Celula

---
+++ ED_Pilha/src/Celula.java
@@ -0,0 +1,33 @@
+public class Celula<E>{
+
+    private E item;
+    private Celula<E> proximaCelula;
+
+    public Celula(){
+        this.item = null;
+        setProximaCelula(null);
+
+    }
+
+    public Celula(E item, Celula<E> proximaCelula){
+        this.item = item;
+        this.proximaCelula = proximaCelula;
+    }
+
+    public E getItem() {
+        return item;
+    }
+
+    public void setItem(E item) {
+        this.item = item;
+    }
+
+    public Celula<E> getProximaCelula() {
+        return proximaCelula;
+    }
+
+    public void setProximaCelula(Celula<E> proximaCelula) {
+        this.proximaCelula = proximaCelula;
+    }
+    
+}
